1e339298350d78ef3a6e884bad59ff0e61760aa1
Yinhang Liu <yinhang.liu@intel.com>
2023-02-22 10:23:00 +0800

    msdk: Add function to get the implementation description
    Through the implementation description, we can dynamically obtain
    the codec capabilities supported by the platform.

--- subprojects/gst-plugins-bad/sys/msdk/msdk.h
+++ subprojects/gst-plugins-bad/sys/msdk/msdk.h
@@ -107,6 +107,7 @@ typedef struct _GstMsdkSurface GstMsdkSurface;
 
 struct _MsdkSession
 {
+  mfxU32 impl_idx;
   mfxSession session;
   mfxLoader loader;
 };
@@ -160,6 +161,11 @@ msdk_get_platform_codename (mfxSession session);
 mfxStatus
 msdk_init_msdk_session (mfxIMPL impl, mfxVersion * pver,
     MsdkSession * msdk_session);
+
+gpointer
+msdk_get_impl_description (const mfxLoader * loader, mfxU32 impl_idx);
+gboolean
+msdk_release_impl_description (const mfxLoader * loader, gpointer impl_desc);
 
 G_END_DECLS
 
